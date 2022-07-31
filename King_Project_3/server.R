39a2663c619faa3cce688828e064ebffd0a5fd7f
twk0101 <twking@ncsu.edu>
2022-07-31 13:28:50 -0400

    Added structure to data & modeling pages

--- King_Project_3/server.R
+++ King_Project_3/server.R
@@ -1,28 +1,27 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(caret)
+library(DT)
+library(tidyverse)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+    #Read in data
+    data_raw <- read.csv("baseball_data.csv")
+  
+    data_selected <- reactive({
+      cols <- input$columns
+      data_selected <- data_raw[,cols]
     })
+    
+    output$dataTable <- DT::renderDataTable({
+      data_selected()
+    })
+    
+    output$downloadData <- downloadHandler(
+      filename = "baseball.csv",
+      content = function(file) {
+        write.csv(data_selected(), file, row.names = FALSE)
+      }
+    )
 
 })
--- King_Project_3/ui.R
+++ King_Project_3/ui.R
@@ -1,7 +1,9 @@
 library(shiny)
 library(shinydashboard)
+library(shinythemes)
+library(mathjaxr)
 
-shinyUI(dashboardPage(
+shinyUI(dashboardPage(skin = "purple",
   dashboardHeader(title = "Baseball Stuff"),
   
   dashboardSidebar(
@@ -15,16 +17,63 @@ shinyUI(dashboardPage(
   
   dashboardBody(
     tabItems(
-      tabItem(tabName = "about"),
+      tabItem(tabName = "about",
+                fluidPage(
+                  h2("Hit the Ball Hard"),
+                  br(),
+                  p("While baseball is by no means a simple game, it should be noted that unlike in many
+                    other sports, the baseball player's main objective can be described quite simply - hit the ball hard.
+                    Baseball is also unique in the fact that so many of its on-field events can be isolated which leads to 
+                    increased opporunities for (sometimes over-) analyization. ")
+                )
+              
+              ),
       tabItem(tabName = "exploration"),
       tabItem(tabName = "modeling",
               tabsetPanel(
-                tabPanel("Modeling Info","content"),
+                tabPanel("Modeling Info",
+                          fluidPage(
+                            titlePanel("Model Information and Descriptions"),
+                            br(),
+                            h4("Multiple Linear Regression"),
+                            br(),
+                            p("Multiple linear regression is a modeling technique where "),
+                            br(),
+                            h4("Regression Tree"),
+                            br(),
+                            p("A regression tree is a tree that does "),
+                            br(),
+                            h4("Random Forest"),
+                            br(),
+                            p("Random forest is an ensamble modeling technique that "),
+                            withMathJax(sprintf('$$3^2+4^2=5^2$$')),
+                           
+                          )
+                        ),
                 tabPanel("Model Fitting", "content"),
                 tabPanel("Prediction","content")
               )
               ),
-      tabItem(tabName = "data")
+      tabItem(tabName = "data",
+                fluidPage(
+                  titlePanel("Explore the Data Set"),
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput("columns","Select Columns to View", choices=list("Season", "Name", "EV", "maxEV", "LA", "BarrelPercent", "HardHitPercent","wOBA"),
+                                   multiple = TRUE, selected = list("Season", "Name")),
+                      submitButton("Generate Table"),
+                      br(),
+                      p("Download a .csv with the columns you've selected:"),
+                      downloadButton("downloadData", "Download")
+                    ),
+                    mainPanel(
+                      dataTableOutput("dataTable")
+                    )
+                  )
+                  
+                
+                )
+              )
       )
       
       
